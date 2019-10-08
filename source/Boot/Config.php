22a8fea75f7ce3096fc3475bc76e8d4993a3eaff
Rogerio Maateus Ricardo <rogerio22ricardo@gmail.com>
2019-10-08 11:13:49 -0300

    Iniciando API

---
+++ source/Boot/Config.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * DATABASE
+ */
+define("CONF_DB_HOST", "localhost");
+define("CONF_DB_USER", "root");
+define("CONF_DB_PASS", "");
+define("CONF_DB_NAME", "fullstackphp");
+
+/**
+ * PROJECT URLs
+ */
+define("CONF_URL_BASE", "https://www.cafecontrol.com.br");
+define("CONF_URL_TEST", "https://www.localhost/fsphpproject");
+
+/**
+ * SITE
+ */
+define("CONF_SITE_NAME", "CaféControl");
+define("CONF_SITE_TITLE", "Gerencie suas contas com o melhor café");
+define("CONF_SITE_DESC",
+    "O CafeControl é um gerenciador de contas simples, poderoso e gratuito. O prazer de tomar um café e ter o controle total de suas contas.");
+define("CONF_SITE_LANG", "pt_BR");
+define("CONF_SITE_DOMAIN", "upinside.com.br");
+define("CONF_SITE_ADDR_STREET", "SC 406 - Rod. Drº Antônio Luiz Moura Gonzaga");
+define("CONF_SITE_ADDR_NUMBER", "3339");
+define("CONF_SITE_ADDR_COMPLEMENT", "Bloco A, Sala 208");
+define("CONF_SITE_ADDR_CITY", "Florianópolis");
+define("CONF_SITE_ADDR_STATE", "SC");
+define("CONF_SITE_ADDR_ZIPCODE", "88048-301");
+
+/**
+ * SOCIAL
+ */
+define("CONF_SOCIAL_TWITTER_CREATOR", "@");
+define("CONF_SOCIAL_TWITTER_PUBLISHER", "@");
+define("CONF_SOCIAL_FACEBOOK_APP", "626590460695980");
+define("CONF_SOCIAL_FACEBOOK_PAGE", "upinside");
+define("CONF_SOCIAL_FACEBOOK_AUTHOR", "");
+define("CONF_SOCIAL_GOOGLE_PAGE", "107305124528362639842");
+define("CONF_SOCIAL_GOOGLE_AUTHOR", "103958419096641225872");
+define("CONF_SOCIAL_INSTAGRAM_PAGE", "");
+define("CONF_SOCIAL_YOUTUBE_PAGE", "upinside");
+
+/**
+ * DATES
+ */
+define("CONF_DATE_BR", "d/m/Y H:i:s");
+define("CONF_DATE_APP", "Y-m-d H:i:s");
+
+/**
+ * PASSWORD
+ */
+define("CONF_PASSWD_MIN_LEN", 8);
+define("CONF_PASSWD_MAX_LEN", 40);
+define("CONF_PASSWD_ALGO", PASSWORD_DEFAULT);
+define("CONF_PASSWD_OPTION", ["cost" => 10]);
+
+/**
+ * VIEW
+ */
+define("CONF_VIEW_PATH", __DIR__ . "/../../shared/views");
+define("CONF_VIEW_EXT", "php");
+define("CONF_VIEW_THEME", "cafeweb");
+define("CONF_VIEW_APP", "cafeapp");
+define("CONF_VIEW_ADMIN", "cafeadm");
+
+/**
+ * UPLOAD
+ */
+define("CONF_UPLOAD_DIR", "storage");
+define("CONF_UPLOAD_IMAGE_DIR", "images");
+define("CONF_UPLOAD_FILE_DIR", "files");
+define("CONF_UPLOAD_MEDIA_DIR", "medias");
+
+/**
+ * IMAGES
+ */
+define("CONF_IMAGE_CACHE", CONF_UPLOAD_DIR . "/" . CONF_UPLOAD_IMAGE_DIR . "/cache");
+define("CONF_IMAGE_SIZE", 2000);
+define("CONF_IMAGE_QUALITY", ["jpg" => 75, "png" => 5]);
+
+/**
+ * MAIL
+ */
+define("CONF_MAIL_HOST", "smtp.sendgrid.net");
+define("CONF_MAIL_PORT", "******************");
+define("CONF_MAIL_USER", "******************");
+define("CONF_MAIL_PASS", "******************");
+define("CONF_MAIL_SENDER", ["name" => "Rgerio Mateus Ricardo", "address" => "rogerio22mateus@outlook.com"]);
+define("CONF_MAIL_SUPPORT", "rogerio22ricardo@gmail.com");
+define("CONF_MAIL_OPTION_LANG", "br");
+define("CONF_MAIL_OPTION_HTML", true);
+define("CONF_MAIL_OPTION_AUTH", true);
+define("CONF_MAIL_OPTION_SECURE", "tls");
+define("CONF_MAIL_OPTION_CHARSET", "utf-8");
+
+/**
+ * PAGAR.ME
+ */
+define("CONF_PAGARME_MODE", "test");
+define("CONF_PAGARME_LIVE", "******************");
+define("CONF_PAGARME_TEST", "******************");
+define("CONF_PAGARME_BACK", CONF_URL_BASE . "/pay/callback");
